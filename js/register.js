d53eade64fa53c9a0a3e410af145c0da60ef6ee1
buhtib <2418531916@qq.com>
2018-10-09 00:00:45 +0800

    no js and start.html not success

--- js/register.js
+++ js/register.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
-   $('.drop').click(function(){
-    $('.drop_content').css("display","block");
-   })
-
+$(document).ready(function () {
+    // 点击触发click
+    $('.drop').click(function (event) {
+        // 下拉框的隐藏和显示
+        $('.drop_content').toggle();
+        // target赋值
+        var $target = $(event.target);
+        // 如果点击的target是li，将他子元素span，html内容赋值给上面（span没有占满li 两边会点击到li）
+        if ($target.is("li")) {
+            $(this).children('span').html($target.children('span').html())
+        // 如果点击的target是li里的span，html内容赋值给上面
+        } else if ($target.is("span")) {
+            $(this).children('span').html($target.html())
+        }
+    })
 })
-
-
-
-
