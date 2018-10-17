0c76bdb1380c55fcf96532319838d49115e2a3ee
buhtib <2418531916@qq.com>
2018-10-18 00:16:55 +0800

    首页 商品页  导航小购物车显示数据（未完善）

--- js/addToCar.js
+++ js/addToCar.js
@@ -23,6 +23,10 @@ var sendData = (function(){
             // 跳转页面会清空 arr
             if(localStorage.car_data) {
                 _this.arr = JSON.parse(localStorage.car_data);
+                _this.nav_car(_this.arr)
+            }else {
+                $('.localdata').html(` <img src="img/car.png" alt="">
+                你的购物车是空的，赶紧选购吧~`)
             }
              // 每次点击加入一个对象
              var obj = {
@@ -50,6 +54,41 @@ var sendData = (function(){
             localStorage.car_data = JSON.stringify(_this.arr);
             
         },
+
+        nav_car:function (arr) {
+            var arr = [];
+            var allprice;
+            var head = `<ul class="localdata_top">`
+            arr.push(head);
+
+            for(var i = 0; i < arr.length; i++) {
+                var sec = ` <li>
+                <em><i class="shop hw-xiaogou"></i></em>
+                
+                <div class="pro_section clear">
+                    <img src="img/car_product.png" alt="">
+                    <p>HUAWEI P20 6GB+64GB 全网通版（极光色）</p>
+                    <div class="pro_">
+                        <span class="pro_price"> ¥ ${arr[i].price}</span><span class="pro_num"> x ${arr[i].num}</span>
+                    </div>
+                </div>
+            </li>`
+             allprice += arr[i].price;
+                arr.push(sec)
+            }
+            var foot = `</ul>
+            <div class="localdata_down">
+                <div class="total_price_list">
+                    <span >总计：</span>
+                    <h2 class="total_price">¥ ${allprice}</h2>
+                </div>
+                
+                <a href="login.html">结算</a>
+            </div>`
+            arr.push(foot)
+            $('.localdata').html(arr.join(''))
+            // $('.localdata').show()
+        }
 
       
     }
